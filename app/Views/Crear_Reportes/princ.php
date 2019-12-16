9e36e54567b91aff2d418694f8e830c14ba1077e
AnaAizprua <anagab.aizprua@gmail.com>
2019-12-15 21:25:48 -0500

    Update princ.php

--- app/Views/Crear_Reportes/princ.php
+++ app/Views/Crear_Reportes/princ.php
@@ -11,78 +11,21 @@
   <meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
 	<link href="/bootstrap/css/dashboard.css" rel="stylesheet">
-
 </head>
 
 <body>
-  <!-- Page Content -->
-  
-  <div class="container">
-  <br><br><br>
-  <header>
-    <div clas="row">
-      <button type="button" class="btn btn-outline-dark" onclick="location.href='?controller=Login&&action=index'">Iniciar Sesión</button>
-      <img alt="Responsive image" class="img-fluid rounded" src="Imagenes/logo.jpeg">
+
+<div class="container">
+ <div class="text-center py-5">
+  <div class="card text-center">
+    <div class="card-body">
+      <h5 class="card-title">Reporte realizado y enviado con exito</h5><br>
+      <a href="#" class="btn btn-primary">Realizar Otro Reporte</a>
+      <a href="#" class="btn btn-primary">Volver a inicio</a>
     </div>
-    <div class="row">
-      <div class="col">
-        <h1 class="my-4">Bienvenidos a SAFISC</h1>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-11">
-        <h2 class="my-2">Sistema de Administración de la Facultad de Ingeniería de Sistemas Computacionales</h2>
-      </div>
-    </div>
-    <!-- Marketing Icons Section -->
-    <div class="row">
-      <div class="col-lg-4 mb-4">
-        <div class="card h-100">
-          <h4 class="card-header">Misión</h4>
-          <div class="card-body">
-            <p class="card-text" >
-                La Secretaría Administrativa de la Facultad de Ingeniería de Sistemas 
-                Computacionales es la responsable de la administración de los recursos de la Facultad, sean estos económicos, 
-                de infraestructura y humanos; sirviendo de apoyo a la gestión de las autoridades de la 
-                Facultad con un adecuado desempeño de nuestras responsabilidades procurando un desarrollo 
-                paralelo a la evolución de las técnicas y metodologías gerenciales.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 mb-4">
-        <div class="card h-100">
-          <h4 class="card-header">Visión</h4>
-          <div class="card-body">
-            <p class="card-text">
-              La Secretaría Administrativa cuenta con el personal necesario para la ejecución de las tareas a ella asignadas: 
-              las cuales son ejecutadas con el apoyo de modernos sistemas autorizados, garantizando un desempeño óptimo y un 
-              rendimiento excelente del recurso humano en un ambiente laboral adecuado y cónsono con las exigencias del puesto.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 mb-4">
-        <div class="card h-100">
-          <h4 class="card-header">Valores</h4>
-          <div class="card-body">
-            <ul>
-              <li>Personal capacitado para desarrollar las funciones</li>
-              <li>Buen trabajo en equipo</li>
-              <li>Voluntad para hacer las cosas bien</li>
-              <li>Responsabilidad</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    </header>
   </div>
-  
-  <!-- /.container -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ </div>
+</div>
 
 </body>
 
